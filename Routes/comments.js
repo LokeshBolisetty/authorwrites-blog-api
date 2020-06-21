622c7e2114ae36d926555c6162d5d29dfbd5751d
Lokesh Bolisetty <63059375+LokeshBolisetty@users.noreply.github.com>
2020-06-21 16:02:51 +0530

    Updated Comments

--- Routes/comments.js
+++ Routes/comments.js
@@ -3,6 +3,61 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const notes = require("../models/notes");
 
+router.put("/:id", (req, res, next) => {
+  const id = req.params.id;
+  const newC = req.body.Comments;
+  let commenter;
+  notes
+    .findById(id)
+    .select("Comments")
+    .exec()
+    .then((doc) => {
+      console.log("doc=" + doc);
+      commenter = doc.Comments;
+      console.log("commenter=" + commenter);
+      commenter.push(newC);
+
+      notes
+        .update({ _id: id }, { Comments: commenter })
+        .exec()
+        .then((result) => {
+          console.log(result);
+          res.status(200).json({ message: "entry updated" });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({ error: err });
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+
+module.exports = router;
+
+/*
+router.post("/:id", (req, res, next) => {
+  const id = req.params.id;
+  const newC = req.body.Comments;
+  let commenter = [];
+  notes
+    .findById(id)
+    .exec()
+    .then((result) => {
+      console.log(result.Comments);
+      res.status(200).json(result.Comments);
+      updateOne({_id:id},{result.Comments = result.Comments.push(newC)})
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: "Was not able to post your comment to the comments list",
+      });
+    });
+});
+
 router.post("/:id", (req, res, next) => {
   const id = req.params.id;
   const newC = req.body.Comments;
@@ -39,5 +94,5 @@ router.post("/:id", (req, res, next) => {
     }
   );
 });
-
+*/
 module.exports = router;
