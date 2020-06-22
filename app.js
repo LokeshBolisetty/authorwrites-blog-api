6e7516232879f4d472c692fb8a9046173404b318
Lokesh Bolisetty <63059375+LokeshBolisetty@users.noreply.github.com>
2020-06-22 15:55:40 +0530

    Added Cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const router = require("./Routes/noter");
 const nameRoutes = require("./Routes/name");
 const commentRoutes = require("./Routes/comments");
+const cors = require("cors");
 
 mongoose.connect(
   "mongodb+srv://Lokesh:Stbemhs@blogdata-y4ixx.mongodb.net/BlogData?retryWrites=true&w=majority",
@@ -16,6 +17,7 @@ mongoose.connect(
 
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
+app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
