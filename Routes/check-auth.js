863bb938c48f760d7703d390db256af6300b715b
Lokesh Bolisetty <63059375+LokeshBolisetty@users.noreply.github.com>
2020-06-24 19:02:21 +0530

    Changed to headers

--- Routes/check-auth.js
+++ Routes/check-auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.body.token;
+    const token = req.headers.authorization.split(" ")[1];
     console.log(token);
     const decoded = jwt.verify(token, "password");
     req.userData = decoded;
